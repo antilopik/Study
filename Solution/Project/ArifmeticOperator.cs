c2fce2b9b31e7d709215921e9d68ee6b1e9177a6
andre <andre@DESKTOP-JHN4J3G>
2025-03-28 16:55:13 +0200

    added GetOperationFromConsole

---
+++ Solution/Project/ArifmeticOperator.cs
@@ -0,0 +1,10 @@
+﻿namespace Project
+{
+    internal enum ArifmeticOperator
+    {
+        Minus = 0,
+        Plus = 1,
+        Multiply = 2,
+        Divide = 3
+    }
+}
--- Solution/Project/Program.cs
+++ Solution/Project/Program.cs
@@ -6,24 +6,24 @@ class Calculator
     {
         var firstNumber = GetNumberFromConsole("number");
         var secondNumber = GetNumberFromConsole("second number");
-        Console.WriteLine("please enter operator");
-        var action = Console.ReadLine();
-        if (action == "+")
+        var operation = GetOperationFromConsole(out var operatorasstring);
+        float result = 0f;
+        switch (operation)
         {
-            Console.WriteLine("Result: " + (firstNumber + secondNumber));
+            case ArifmeticOperator.Plus:
+              result = firstNumber + secondNumber;
+                break;
+            case ArifmeticOperator.Minus:
+                result = firstNumber - secondNumber;
+                break;
+            case ArifmeticOperator.Multiply:
+                result = firstNumber * secondNumber;
+                break;
+            case ArifmeticOperator.Divide:
+                result = firstNumber / secondNumber;
+                break;
         }
-        else if (action == "-")
-        {
-            Console.WriteLine("Result: " + (firstNumber - secondNumber));
-        }
-        else if (action == "*")
-        {
-            Console.WriteLine("Result: " + (firstNumber * secondNumber));
-        }
-        else if (action == "/")
-        {
-            Console.WriteLine("Result: " + (firstNumber / secondNumber));
-        }
+        Console.WriteLine($"{firstNumber} {operatorasstring} {secondNumber} = {result}"); 
     }
 
     private static int GetNumberFromConsole(string whatToAskFromUser)
@@ -31,12 +31,49 @@ class Calculator
         Console.WriteLine($"Please enter a {whatToAskFromUser}");
         var arg = Console.ReadLine();
         int result;
-        while (!int.TryParse(arg, out result)) 
+        while (!int.TryParse(arg, out result))
         {
             Console.WriteLine($"'{arg}' is not a number! Please enter a number");
             arg = Console.ReadLine();
         }
 
         return result;
+
+    }
+
+    private static ArifmeticOperator GetOperationFromConsole(out string action)
+    {
+        var operatorIsParsed = false;
+        while (!operatorIsParsed)
+        {
+            Console.WriteLine("please enter operator");
+            action = Console.ReadLine();
+            if (action == "+")
+            {
+                operatorIsParsed = true;
+                return ArifmeticOperator.Plus;  
+            }
+            else if (action == "-")
+            {
+                operatorIsParsed = true;
+                return ArifmeticOperator.Minus;   
+            }
+            else if (action == "*")
+            {
+                operatorIsParsed = true;
+                return ArifmeticOperator.Multiply;
+            }
+            else if (action == "/")
+            {
+                operatorIsParsed = true;
+                return ArifmeticOperator.Divide;    
+            }
+            else
+            {
+                operatorIsParsed = false;
+                Console.WriteLine("Invalid operator. Please enter a valid operator (+, -, *, /)");
+            }
+        }
+        throw new Exception("This should never happen");
     }
 }

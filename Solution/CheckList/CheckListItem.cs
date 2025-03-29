5864b0ba1faad022177e00ac7ef0c5d99af494ff
andre <andre@DESKTOP-JHN4J3G>
2025-03-29 14:46:37 +0200

    Simple checklist

---
+++ Solution/CheckList/CheckListItem.cs
@@ -0,0 +1,14 @@
+﻿namespace CheckList
+{
+    internal class CheckListItem
+    {
+        public readonly string title;
+
+        public bool isDone;
+
+        public CheckListItem(string title)
+        {
+            this.title = title;
+        }
+    }
+}
---
+++ Solution/CheckList/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Transactions;
+using CheckList;
+
+var list = new List<CheckListItem>();
+
+Console.WriteLine("Add new task?");
+var arg = Console.ReadLine();
+while (arg == "yes")
+{
+    Console.WriteLine("Enter name of task");
+    var tittle = Console.ReadLine();
+    var newCheckListItem = new CheckListItem(tittle);
+    list.Add(newCheckListItem);
+    Console.WriteLine("Add new task?");
+    arg = Console.ReadLine();
+}
+
+if (arg == "list")
+{
+    foreach (var checkListItem in list)
+    {
+        Console.WriteLine(checkListItem.title);
+    }
+}
+

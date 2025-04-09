9ddd16219cf9607e6149a078c75e9f00edcfe243
andre <andre@DESKTOP-JHN4J3G>
2025-04-09 18:04:15 +0300

    Extracted methods

--- Solution/Animals/Program.cs
+++ Solution/Animals/Program.cs
@@ -1,38 +1,41 @@
 ﻿using System.Collections.Generic;
 using Animals.Models;
-
+// declare variables to store entities for both sides of the river
 var from = new List<BioEntity> { new Cabbage(), new Sheep(), new Wolf() };
 var to = new List<BioEntity>(3);
-
+// declare a mapping for user input to entity types
 var cargosDictionary = new Dictionary<string, Type>
 {
-    { "Cabbage", typeof(Cabbage) },
-    { "Sheep", typeof(Sheep) },
-    { "Wolf", typeof(Wolf) }
+    { "cabbage", typeof(Cabbage) },
+    { "sheep", typeof(Sheep) },
+    { "wolf", typeof(Wolf) }
 };
 
-Type currentCargoType = null;
-while (currentCargoType == null)
-{
-    Console.WriteLine("Enter first cargo");
-    var firstCargo = Console.ReadLine();
-    TryParseCargo(firstCargo, cargosDictionary, out currentCargoType);
-}
+Type currentCargoType = GetFirstEnityTypeToCarry(cargosDictionary);
+MakeTransportation(from, to, currentCargoType);
+CheckLooseConditions(to, from);
 
-for (int i = 0; i < from.Count; i++)
+static void CheckLooseConditions(List<BioEntity> to, List<BioEntity> from)
 {
-    if (from[i].GetType() == currentCargoType)
+    if (CheckLooseCondition(to) == GameState.Lost || CheckLooseCondition(from) == GameState.Lost)
     {
-        var cargo = from[i];
-        from.Remove(cargo);
-        to.Add(cargo);
+        Console.WriteLine("You lost");
+        return;
     }
 }
 
-if (CheckLooseCondition(to) == GameState.Lost || CheckLooseCondition(from) == GameState.Lost)
+
+static void MakeTransportation(List<BioEntity> from, List<BioEntity> to, Type typeToCarry)
 {
-    Console.WriteLine("You lost");
-    return;
+    for (int i = 0; i < from.Count; i++)
+    {
+        if (from[i].GetType() == typeToCarry)
+        {
+            var cargo = from[i];
+            from.Remove(cargo);
+            to.Add(cargo);
+        }
+    }
 }
 
 static GameState CheckLooseCondition(List<BioEntity> list)
@@ -59,7 +62,7 @@ static GameState CheckLooseCondition(List<BioEntity> list)
 static bool TryParseCargo(string cargo, Dictionary<string, Type> cargosDictionary, out Type? typeOfCargo)
 {
     typeOfCargo = null;
-    if (cargosDictionary.TryGetValue(cargo, out var type))
+    if (cargosDictionary.TryGetValue(cargo.ToLower(), out var type))
     {
         typeOfCargo = type;
     }
@@ -67,3 +70,15 @@ static bool TryParseCargo(string cargo, Dictionary<string, Type> cargosDictionar
     return typeOfCargo != null;
 }
 
+static Type GetFirstEnityTypeToCarry(Dictionary<string, Type> cargosDictionary)
+{
+    Type currentCargoType = null;
+    while (currentCargoType == null)
+    {
+        Console.WriteLine("Enter first cargo");
+        var firstCargo = Console.ReadLine();
+        TryParseCargo(firstCargo, cargosDictionary, out currentCargoType);
+    }
+
+    return currentCargoType;
+}

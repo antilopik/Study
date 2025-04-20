6178a6a1da79d69869e3c84b987122a9c2ac47d9
andre <andre@DESKTOP-JHN4J3G>
2025-04-20 13:49:14 +0300

    File write example

---
+++ Solution/FilesInteraction/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a line:");
+
+const string fileName = "text.txt";
+if(!File.Exists(fileName))
+{
+    var stream = File.Create(fileName);
+    stream.Dispose();
+}
+
+var exit = false;
+while (!exit)
+{
+    var input = Console.ReadLine();
+    File.AppendAllLines(fileName, new string[] { input });
+    Console.WriteLine("Add more?");
+    exit = Console.ReadLine() != "yes";
+}

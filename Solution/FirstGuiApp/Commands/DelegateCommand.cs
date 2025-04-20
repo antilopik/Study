f625622fe21e633722408486493b182331d2ddb3
andre <andre@DESKTOP-JHN4J3G>
2025-04-20 14:25:49 +0300

    GUI app demp

---
+++ Solution/FirstGuiApp/Commands/DelegateCommand.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Input;
+
+namespace FirstGuiApp.Commands
+{
+    internal class DelegateCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+        private readonly Action<object?> _function;
+        public DelegateCommand(Action<object?> function)
+        {
+            _function = function;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _function(parameter);
+        }
+    }
+}
---
+++ Solution/FirstGuiApp/ViewModels/PaymentsRegistrationViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using FirstGuiApp.Commands;
+
+namespace FirstGuiApp.ViewModels
+{
+    public class PaymentsRegistrationViewModel : INotifyPropertyChanged
+    {
+        private string _address;
+
+        public string Address
+        {
+            get => _address;
+            set 
+            {
+                if (!string.Equals(value, _address))
+                { 
+                    _address = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Address)));
+                }
+            }
+        }
+
+        public string Period { get; set; }
+        public decimal Amount { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public PaymentsRegistrationViewModel()
+        {
+            Command = new DelegateCommand(OnButtonClick);
+        }
+
+        public ICommand Command { get; }
+
+        private void OnButtonClick(object param)
+        {
+            Address = "Valitie";
+        }
+    }
+}

a95f55bc8d5fc0dff5d6dc01e671bd9f11c98fdc
andre <andre@DESKTOP-JHN4J3G>
2025-05-03 18:21:34 +0300

    Progress

---
+++ Solution/HomeWork/Behaviors/AllowSelectionDirectionBehavior.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+using HomeWork.Models;
+using Microsoft.Xaml.Behaviors;
+
+namespace HomeWork.Behaviors
+{
+    public sealed class AllowSelectionDirectionBehavior : Behavior<Selector>
+    {
+        public Direction AllowedDirection { get; set; }
+
+        public Direction CurrentDirection
+        {
+            get { return (Direction)GetValue(CurrentDirectionProperty); }
+            set { SetValue(CurrentDirectionProperty, value); }
+        }
+
+        public static readonly DependencyProperty CurrentDirectionProperty =
+            DependencyProperty.Register(nameof(CurrentDirection), typeof(Direction), typeof(AllowSelectionDirectionBehavior), new PropertyMetadata(Direction.LeftToRight));
+
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.SelectionChanged += AssociatedObject_SelectionChanged;
+        }
+
+        private void AssociatedObject_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (CurrentDirection != AllowedDirection)
+            {
+                AssociatedObject.SelectedIndex = -1;
+            }
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.SelectionChanged -= AssociatedObject_SelectionChanged;
+        }
+    }
+}

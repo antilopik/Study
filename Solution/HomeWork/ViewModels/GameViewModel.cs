bde33a813b254d038129429aba6b1eb7ea550d61
andre <andre@DESKTOP-JHN4J3G>
2025-05-07 19:08:31 +0300

    Game win or loose conditions check

--- Solution/HomeWork/ViewModels/GameViewModel.cs
+++ Solution/HomeWork/ViewModels/GameViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows;
 using HomeWork.Models;
 using Model.View.ViewModel.Base;
 using Model.View.ViewModel.Base.Commands;
@@ -7,12 +8,15 @@ namespace HomeWork.ViewModels
 {
     internal sealed class GameViewModel : ViewModelBase
     {
-        public ObservableCollection<BioEntity> LeftSide { get; } = new ObservableCollection<BioEntity>()
+        public static readonly List<BioEntity> animals = new List<BioEntity>()
         {
             { new Wolf() },
             { new Cabbage() },
             { new Sheep() }
         };
+
+
+        public ObservableCollection<BioEntity> LeftSide { get; } = new ObservableCollection<BioEntity>(animals);
 
         public ObservableCollection<BioEntity> RightSide { get; } = new ObservableCollection<BioEntity>();
 
@@ -34,16 +38,92 @@ namespace HomeWork.ViewModels
         {
             if (RouteDirection == Direction.LeftToRight)
             {
-                RightSide.Add(SelectedPassanger);
-                LeftSide.Remove(SelectedPassanger);
+                if (SelectedPassanger != null)
+                {
+                    RightSide.Add(SelectedPassanger);
+                    LeftSide.Remove(SelectedPassanger);
+                }
+                // тут пока еше RouteDirection это текущее положение лодочника
+                if (RightSide.Count == 3)
+                {
+                    if (MessageBox.Show("Do you want to restart?", "You won!", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                    {
+                        RestartGame();
+                    }
+                    else
+                    {
+                        Application.Current.Shutdown();
+                    }
+                    return;
+                }
+                else if (IsGameLost(LeftSide))
+                {
+                    ProcessLoose();
+                    return;
+                }
+                //после следующей строки RouteDirection это куда лодочник поплывет в следующий раз
                 RouteDirection = Direction.RightToLeft;
             }
             else
             {
-                LeftSide.Add(SelectedPassanger);
-                RightSide.Remove(SelectedPassanger);
+                if (SelectedPassanger != null)
+                {
+                    LeftSide.Add(SelectedPassanger);
+                    RightSide.Remove(SelectedPassanger);
+                }
+                // тут пока еше RouteDirection это текущее положение лодочника
+                if (IsGameLost(RightSide))
+                {
+                    ProcessLoose();
+                    return;
+                }
+                //после следующей строки RouteDirection это куда лодочник поплывет в следующий раз
                 RouteDirection = Direction.LeftToRight;
             }
+        }
+
+        private void ProcessLoose()
+        {
+            if (MessageBox.Show("Do you want to restart?", "You lost!", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                RestartGame();
+            }
+            else
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        static bool IsGameLost(ObservableCollection<BioEntity> currentSide)
+        {
+            for (int i = 0; i < currentSide.Count; i++)
+            {
+                BioEntity current = currentSide[i];
+                for (int j = i + 1; j < currentSide.Count; j++)
+                {
+                    if (current.CanEat(currentSide[j]))
+                    {
+                        Console.WriteLine($"{current} has eaten {currentSide[j]}");
+                        return true;
+                    }
+                    else if (currentSide[j].CanEat(current))
+                    {
+                        Console.WriteLine($"{currentSide[j]} has eaten {current}");
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private void RestartGame()
+        {
+            SelectedPassanger = null;
+            RouteDirection = Direction.LeftToRight;
+            RightSide.Clear();
+            LeftSide.Clear();
+            animals.ForEach(x => LeftSide.Add(x));
         }
     }
 }
